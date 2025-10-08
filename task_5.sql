cfe1c14c06dd74214d384db3cb2b4fda2281fb84
Islam Muhammad Atallah <59668025+IslamMoAtallah@users.noreply.github.com>
2025-10-08 13:13:37 +0300

    Update task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -1,2 +1,3 @@
-INSERT INTO Customer (customer_id, customer_name, email, address)
+-- INSERT A SINGLE ROW INTO THE CUSTOMERS TABLE
+INSERT INTO Customers (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
